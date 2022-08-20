a729f0827335d65b781bade988b22950ce7cc158
unknown <riyadhasanre@gmail.comgit config --global user.name>
2022-08-20 22:47:38 +0600

    pin-put-box

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log("app.js file add nice and thid is vwery nice");
+function getpin() {
+    const pin = ganaretpinset();
+    const pinstring = pin + '';
+    if(pinstring.length === 4){
+        return pin;
+    }
+    else {
+        // console.log('pin is not 4 digit');
+        return getpin();
+    }
+}
+
+
+function ganaretpinset() {
+
+    const random = Math.round(Math.random() * 10000);
+    return random;
+}
+document.getElementById('getpin-set').addEventListener('click', function () {
+    const pin = getpin();
+    // console.log(pin);
+    // display pin 
+    const displaypin = document.getElementById('get-pin-box');
+    displaypin.value = pin;
+})
